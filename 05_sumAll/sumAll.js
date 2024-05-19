e98c7aa4592e8a928b6e52985967b885459b33b6
Jasper Alblas <just_jasper@hotmail.com>
2024-05-19 20:58:56 +0200

    Adds remaining solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    const args = Array.from(arguments);
 
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+
+    args.sort(compareNumbers);
+
+    console.log(args[0])
+    console.log(args[1])
+
+    if (args[0] < 0 || args[1] < 0 || !Number.isInteger(args[0]) || !Number.isInteger(args[1])) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    for (let index = args[0]; index <= args[1]; index++) {
+        sum += index;
+
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // x °F ≘ (x − 32) × 5 / 9°C
+  let celcius = (fahrenheit - 32) * 5 / 9;
+  return Math.round(celcius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  // x °C ≘ (x × 9 / 5 + 32) °F
+  let fahrenheit = (celcius * 9 / 5 + 32);
+  return Math.round(fahrenheit * 10) / 10
 };
 
 // Do not edit below this line
