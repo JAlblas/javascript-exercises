e6da0a0cc06452d414e9abafa4ed5d729b37bd0b
Jasper Alblas <just_jasper@hotmail.com>
2024-05-19 11:00:16 +0200

    first 4 js TDD challnges solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, element) {
+    const args = Array.from(arguments);
 
+    for (let i = 1; i < args.length; i++) {
+        const element = args[i];
+
+        while (array.indexOf(element) != -1) {
+            const index = array.indexOf(element);
+            array.splice(index, 1);
+        }
+
+    }
+
+
+
+    return array;
 };
 
 // Do not edit below this line
