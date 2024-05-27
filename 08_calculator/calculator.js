5cc041dafa10dc8af87b76f37e3922fe17b84c74
Jasper Alblas <just_jasper@hotmail.com>
2024-05-27 21:23:54 +0200

    Adds calculator assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((sum, i) => sum += i, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((sum, i) => sum *= i, 1)
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let factorial = 1;
+  for (let index = 1; index <= x; index++) {
+    factorial *= index;
+
+  }
+  return factorial;
 };
 
 // Do not edit below this line
