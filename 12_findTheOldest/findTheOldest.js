986f8fd42bda5a263f4c41d5ca3cb32458ddc12e
Jasper Alblas <just_jasper@hotmail.com>
2024-05-28 20:24:16 +0200

    Find the oldest program done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function (persons) {
+    let newPersons = persons.map((person) => {
+        return { "name": person.name, "age": person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth }
+    });
+    let sortedPersons = newPersons.sort((a, b) => b.age - a.age);
+    return sortedPersons[0];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
