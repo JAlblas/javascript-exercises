8b7730a60d26436acbee7c6173fbb21dab9bce6e
Jasper Alblas <just_jasper@hotmail.com>
2024-05-28 17:28:15 +0200

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strings = str.replace(/[^a-z0-9]/gi, '').toLowerCase().split('');
+    let reverse = [...strings].reverse();
+    console.log(strings.join(''));
+    console.log(reverse.join(''));
+    return strings.join('') == reverse.join('');
 };
 
 // Do not edit below this line
