c220aa5a03bb0c2a1f9671ac25d1d6d51b47701a
Jasper Alblas <just_jasper@hotmail.com>
2024-05-28 18:07:20 +0200

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (n == 0) return 0;
 
+    let array = [1, 1];
+    for (let index = 2; index < n; index++) {
+        array.push(array[index - 2] + array[index - 1]);
+    }
+    console.log(array);
+    return array[n - 1];
 };
 
 // Do not edit below this line
